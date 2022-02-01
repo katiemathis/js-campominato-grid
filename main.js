cd389d957211f2b9415a4cb21ac6a082480deb6d
katiepalumbo <palumbo.katie@gmail.com>
2022-02-01 11:08:02 +0100

    correzioni

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 
-
-
-
-const gridElement = document.getElementById('easy-grid');
+const gridElement = document.getElementById('grid');
 
 const createGridElement = () => {
     const node = document.createElement('div');
@@ -10,29 +7,62 @@ const createGridElement = () => {
     return node;
 }
 
-let number = document.getElementsByClassName('square').innerHTML
+document.getElementById('start-play').addEventListener('click', play)
 
+function play() {
+    console.log('Lets go');
 
-
-
-
-for (let i = 1; i<=100; i++) {
-
-    const node = createGridElement();
-
-    number += '<p>'+ i +'</p>';
+    const difficoltà = document.getElementById('difficulty').value;
     
+    let numeroCelle;
+    let cellePerRiga;
+
+    switch (difficoltà) {
+        case 'Easy':
+            numeroCelle = 100;
+            break;
+        case 'Hard':
+            numeroCelle = 81;
+            break;
+        case 'Crazy':
+            numeroCelle = 49;
+            break;
+    }
+
+    cellePerRiga = Math.sqrt(numeroCelle);
+
+    const gridElement = document.getElementById('grid');
+
+    grid.innerHTML = '';
+
+    for (let i = 1; i<=numeroCelle; i++) {
+
+        const node = createGridElement();
+
+        const dimensione = `calc(100% /${cellePerRiga})`;
+        node.style.width = dimensione;
+        node.style.height = dimensione;
     
-   
-    node.addEventListener('click', function() {
-        console.log(this);
-        this.classList.add('clicked');
+        let number = document.createTextNode(i);
+        node.appendChild(number)
+        
+        
+       
+        node.addEventListener('click', function() {
+            console.log(this);
+            this.classList.add('clicked');
+    
+        });
+    
+        gridElement.appendChild(node);
+    
+    }
 
-    });
-
-    gridElement.appendChild(node);
 
 }
+
+
+
 
 
 
